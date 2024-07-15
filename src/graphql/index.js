876d250689d1c35c2523468720b087077cdc51a4
Manuel Calle <manuel.calle@bairesdev.com>
2024-07-15 01:19:24 -0500

    feat: #8 Listas y non-null types

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -5,19 +5,28 @@ const {
 
 const typeDefs = `
 type Query {
-  hello: String
+  hello: String!
   getPerson(name: String, age: Int): String
-  getInt(age: Int): Int
+  getInt(age: Int!): Int
   getFloat(price: Float): Float
   getString: String
   getBoolean: Boolean
   getId: ID
+  getNumbers(numbers: [Int!]!): [Int]
 }
 `;
 
+// relation between REST and GQL
+// Get -> query
+// Post, Put, Delete -> mutation
+
+// List on GQL
+// [String]
+// [Int]
+
 const resolvers = {
   Query: {
-    hello: () => 'Hola Mundo',
+    hello: () => null,
     getPerson: (_, args) =>
       `Mi name in ${args.name} and I'm ${args.age} years old`,
     getInt: () => 1,
@@ -25,6 +34,7 @@ const resolvers = {
     getString: () => 'Hola',
     getBoolean: () => true,
     getId: () => '123456',
+    getNumbers: (_, args) => args.numbers,
   },
 };
 
