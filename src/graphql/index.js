0ebc0a5c0c019c4ff448ba54ff31ea5cc7812c4c
Manuel Calle <manuel.calle@bairesdev.com>
2024-07-15 01:40:17 -0500

    feat: #10 Archivos .graphql (best developer experience)

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,37 +2,7 @@ const { ApolloServer } = require('apollo-server-express');
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require('apollo-server-core');
-
-const typeDefs = `
-  type Query {
-    hello: String!
-    getPerson(name: String, age: Int): String
-    getInt(age: Int!): Int
-    getFloat(price: Float): Float
-    getString: String
-    getBoolean: Boolean
-    getId: ID
-    getNumbers(numbers: [Int!]!): [Int]
-    getProduct: Product
-  }
-
-  type Product {
-    id: ID!
-    name: String!
-    price: Float!
-    description: String!
-    image: String!
-    createdAt: String!
-  }
-`;
-
-// relation between REST and GQL
-// Get -> query
-// Post, Put, Delete -> mutation
-
-// List on GQL
-// [String]
-// [Int]
+const { loadFiles } = require('@graphql-tools/load-files');
 
 const resolvers = {
   Query: {
@@ -60,7 +30,7 @@ const resolvers = {
 
 const useGraphql = async (app) => {
   const server = new ApolloServer({
-    typeDefs,
+    typeDefs: await loadFiles('./src/**/*.graphql'),
     resolvers,
     playground: true,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
